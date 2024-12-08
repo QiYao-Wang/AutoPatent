7a0eb3d29a452e081db196f20f33b354c02c7f61
Mathsion <2646769409@qq.com>
2024-12-08 16:07:37 +0800

    Delete scripts directory

--- scripts/download.sh
+++
@@ -1,8 +0,0 @@
-# datasets
-mkdir -p datasets
-
-# outputs
-mkdir -p outputs
-
-# download D2P benchmark test set
-huggingface-cli download --repo-type dataset --resume-download QiYao-Wang/D2P D2P-Benchmark/test.json --local-dir ../datasets
